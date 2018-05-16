7fe958c9bcf265bd7c62c42b4021d481a311128f
bubao <asd565586630@gmail.com>
2018-05-16 11:40:00 +0800

    module

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 /**
  * @author bubao 
- * @description 测试
+ * @description module
  * @date: 2017-04-28 20:00:44
  * @Last Modified by: bubao
- * @Last Modified time: 2018-05-15 18:07:59
+ * @Last Modified time: 2018-05-16 11:39:03
  */
-const config = require('./config.js');
 const zhuanlan = require('./src');
 
-zhuanlan(config.ID)
+module.exports = {
+	zhuanlan
+}
---
+++ test.js
@@ -0,0 +1,11 @@
+/**
+ * @author bubao 
+ * @description 测试
+ * @date: 2017-04-28 20:00:44
+ * @Last Modified by: bubao
+ * @Last Modified time: 2018-05-15 18:07:59
+ */
+const config = require('./config.js');
+const zhuanlan = require('./src');
+
+zhuanlan(config.ID)
