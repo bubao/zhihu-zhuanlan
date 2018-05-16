9d9fe257283e9e363b4be9ef328897e34f47df0b
bubao <asd565586630@gmail.com>
2018-05-16 13:26:31 +0800

    turndown还不如h2m

--- test.js
+++ test.js
@@ -3,9 +3,9 @@
  * @description 测试
  * @date: 2017-04-28 20:00:44
  * @Last Modified by: bubao
- * @Last Modified time: 2018-05-15 18:07:59
+ * @Last Modified time: 2018-05-16 13:21:25
  */
 const config = require('./config.js');
 const zhuanlan = require('./src');
 
-zhuanlan(config.ID)
+zhuanlan(config.ID, './');
