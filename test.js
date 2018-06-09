94c9c4c95c0f34e5e20b9a0c25a3b35c23fa19f9
bubao <asd565586630@gmail.com>
2018-06-09 22:11:02 +0800

    test

--- test.js
+++ test.js
@@ -3,10 +3,10 @@
  * @description 测试
  * @date: 2017-04-28 20:00:44
  * @Last Modified by: bubao
- * @Last Modified time: 2018-06-09 22:06:32
+ * @Last Modified time: 2018-06-09 22:10:53
  */
-import post, { markdown, zhuanlan } from './src';
+import post, { markdown, zhuanlan } from './index';
 const config = require('./config.js');
-post(config.ID, './');
+zhuanlan(config.ID, './');
 // markdown()
 // zhuanlan()
