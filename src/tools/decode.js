f67f85e5f3bcdfaa82f78cc12b6c8af243b2429b
bubao <asd565586630@gmail.com>
2018-11-22 21:55:53 +0800

    重构完成

--- src/tools/decode.js
+++ src/tools/decode.js
@@ -3,34 +3,62 @@
  * @description html内容转markdown
  * @date: 2018-05-15 17:56:12
  * @Last Modified by: bubao
- * @Last Modified time: 2018-11-22 20:25:34
+ * @Last Modified time: 2018-11-22 21:55:25
  */
 const times = require('lodash/times');
 const compact = require('lodash/compact');
 const TurndownService = require('turndown');
-
 const imgsrc = '![](https://pic1.zhimg.com/';
 const Turndown = new TurndownService();
 
-/**
- * Turndown config start
- */
-Turndown.addRule('fencedCodeBlock', {
-	filter: (node, options) => (
-		options.codeBlockStyle === 'fenced' &&
-		node.nodeName === 'PRE' &&
-		node.firstChild &&
-		node.firstChild.nodeName === 'CODE'
-	),
-	replacement: (content, node, options) => (
-		'\n```' + node.firstChild.getAttribute('class') + '\n' +
-		content +
-		'\n```\n'
-	)
+Turndown.addRule('indentedCodeBlock', {
+	filter(node, options) {
+		return (
+			options.codeBlockStyle === 'indented' &&
+			node.nodeName === 'PRE' &&
+			node.firstChild &&
+			node.firstChild.nodeName === 'CODE'
+		);
+	},
+	replacement(content, node) {
+		return `'\n\`\`\`${node.firstChild.getAttribute('class')}\n${content}\n\`\`\`\n`;
+	}
 });
+
 /**
- * Turndown config end
+ * 转换内容，将部分不适合转换的标签改为合适转换的标签
+ * @param {string} content 知乎专栏的Markdown内容
  */
+const replaceContent = (content) => {
+	return content.replace(/<br>/g, '\n').replace(/<code lang="/g, '<pre><code class="language-').replace(/\n<\/code>/g, '\n</code></pre>');
+}
+
+/**
+ * 转换内容，连接转换为绝对连接
+ * @param {string} content 知乎专栏的Markdown内容
+ */
+const replaceImage = (content) => {
+	const reg = /<noscript>.*?<\/noscript>/g;
+	const reg2 = /src="(.*?)"/;
+	let src = content.match(reg);
+	const imageList = [];
+	src = compact(src); // 使用lodash ，即便是src为null也能够转为空的数组
+	times(src.length, (imageNum) => {
+		imageList.push(`\n\n![](${src[imageNum].match(reg2)[1]})\n\n`);
+	});
+	times(src.length, (imageNum) => {
+		content = content.replace(src[imageNum], imageList[imageNum]);
+	});
+	return content.replace(/!\[\]\(/g, imgsrc);
+}
+
+/**
+ * 转换时间
+ * @param {string} time 时间
+ */
+const replaceTime = (time) => {
+	return time.replace("T", ",").replace("+08:00", "");
+}
 
 /**
  * decode
@@ -42,43 +70,25 @@ const decode = (res) => {
 	const jsonObjLength = Object.getOwnPropertyNames(jsonObj).length;
 	return new Promise((resolve) => {
 		times(jsonObjLength, (i) => {
-			jsonObj[i].content = jsonObj[i].content.replace(/<br>/g, '\n').replace(/<code lang="/g, '<pre><code class="language-').replace(/\n<\/code>/g, '\n<\/code><\/pre>');
+			jsonObj[i].content = replaceContent(jsonObj[i].content);
 			let content = Turndown.turndown(jsonObj[i].content);
-			const reg = /<noscript>.*?<\/noscript>/g;
-			const reg2 = /src="(.*?)"/;
-			let src = content.match(reg);
-			const imageList = [];
-			src = compact(src); // 使用lodash ，即便是src为null也能够转为空的数组
-			times(src.length, (imageNum) => {
-				imageList.push(`![](${src[imageNum].match(reg2)[1]})`);
-			});
-			times(src.length, (imageNum) => {
-				content = content.replace(src[imageNum], imageList[imageNum]);
-			});
-			// const pattern = new RegExp("[`~!@#$^&'*()=|{}':;',\\[\\]<>/?~！@#￥……&*（）&mdash;—|{}【】‘；：”“'。，、？]");
-			const pattern = new RegExp("[？、,\\[\\]╲*“”<>|（）]");
-			let rs = '';
-			let { title } = jsonObj[i];
-			times(title.length, (k) => {
-				const rs2 = title.substr(k, 1).replace(/"/, ''); // 使用正则表达式单独去除双引号
-				rs += rs2.replace(pattern, '');
-			});
-			rs = rs.replace("\\\\", '');
-			content = content.replace(/!\[\]\(/g, imgsrc);
+			content = replaceImage(content);
+			const { title } = jsonObj[i];
+			const time = replaceTime(`${jsonObj[i].publishedTime}`);
+			const filename = `${time.split(',')[0]};${filenamify(title)}`;
 
-			const time = `${jsonObj[i].publishedTime}`;
-			const T = time.replace("T", ",").replace("+08:00", "");
-			// const Ti = T.split(',')[0];
-
-			const postId = jsonObj[i].url;
-			const copyRight = `知乎原文: [${title}](https://zhuanlan.zhihu.com${postId})`;
-			// const header = `# ${title}\n\ndate: ${T.replace(",", " ")} `;
+			const postUrl = jsonObj[i].url;
+			const copyRight = `\n\n知乎原文: [${title}](https://zhuanlan.zhihu.com${postUrl})\n\n\n`;
+			const header = `# ${title}\n\ndate: ${time.replace(",", " ")} \n\n\n`;
 			ArrayObj.push({
 				postId,
-				copyRight,
 				title,
+				filename,
+				header,
 				content,
-				time: T
+				copyRight,
+				time: time.split(',')[0],
+				json: res[i]
 			});
 			if (jsonObjLength === ArrayObj.length) {
 				resolve({
