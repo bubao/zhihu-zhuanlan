020c5e1ef1896925b06c55c00a60b4e1896dbb6d
bubao <asd565586630@gmail.com>
2018-11-22 23:58:18 +0800

    v0.0.3

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@
  * @description 专栏
  * @date: 2018-05-15 17:55:58
  * @Last Modified by: bubao
- * @Last Modified time: 2018-11-22 19:53:01
+ * @Last Modified time: 2018-11-22 23:57:52
  */
 
 const Posts = require('./Post');
@@ -12,9 +12,10 @@ const decode = require('./tools/decode');
 /**
  *  知乎专栏抓取器
  * @param {string} postID 知乎专栏的ID
+ * @param {object} spinner ora实例
  */
-function Post(postID) {
-	return Posts(postID).then(decode);
+function Post(postID, spinner) {
+	return Posts(postID, spinner).then(decode);
 };
 
 module.exports = Post;
